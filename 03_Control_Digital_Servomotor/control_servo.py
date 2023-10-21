b837d1a074d90739c41c02b0c32509be49540cd8
WayneKim92 <eunchan0817@gamil.com>
2023-10-21 22:30:44 +0900

    feat: 서보모터 제어
    서보모터: MG995
    최대각도: 0~120
    초당각도: 60

--- 03_Control_Digital_Servomotor/control_servo.py
+++ 03_Control_Digital_Servomotor/control_servo.py
@@ -1,12 +1,10 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo =AngularServo(18, min_angle=0, max_angle=270, min_pulse_width=0.0005, max_pulse_width=0.0025)
+servo = AngularServo(18, min_angle=0, max_angle=120, min_pulse_width=0.0005, max_pulse_width=0.0025)
 
 while (True):
     servo.angle = 0
     sleep(2)
-    servo.angle = 135
-    sleep(2)
-    servo.angle = 260
+    servo.angle = 120
     sleep(2)
