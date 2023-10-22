6d8a36cb36cea2abee549895f9068da4d7af723b
WayneKim92 <eunchan0817@gmail.com>
2023-10-22 08:00:04 -0400

    chore: test

--- 03_Control_Digital_Servomotor/test.py
+++ 03_Control_Digital_Servomotor/test.py
@@ -1,12 +1,29 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(18, min_angle = 0, max_angle = 90)
+# 0~90도 움직임
+# servo = AngularServo(18, min_angle = 0, max_angle = 360)
+# while True:
+#     servo.angle = 0
+#     sleep(3)
+#     servo.angle = 180
+#     sleep(3)
+#     servo.angle = 359
+#     sleep(3)
 
+# 0 ~110도 움직임
+# servo = AngularServo(18, min_angle = 0, max_angle = 180)
+# while True:
+#     servo.angle = 0
+#     sleep(3)
+#     servo.angle = 180
+#     sleep(3)
+
+# 그나마 정확한 수치
+servo = AngularServo(18, min_angle = 0, max_angle = 120)
 while True:
-    for i in range(0,90,1):
-        servo.angle = i
-        sleep(0.1)
-    for i in range(90,0,-1):
-        servo.angle = i
-        sleep(0.1)
+    servo.angle = 0
+    sleep(3)
+    servo.angle = 100
+    sleep(3)
+
